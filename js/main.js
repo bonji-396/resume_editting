3b41021a092f14d8c5158b276afc9882a24c53b6
Shuji Narumi <bonji@mikuro.net>
2021-05-06 05:53:42 +0900

    comment fix

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@ DEFAULT_IS_EDITTING = true;
 /* DOM解析終了イベント時の処理を定義
 ---------------------------------------------------------------------------- */
 document.addEventListener('DOMContentLoaded', () => {
+  // 編集可否を切り替え
   const edittingSwitch = new EdittingSwitch(DEFAULT_IS_EDITTING);
-
+  // 
+  // const reseButton = 
+  const autoLoad = new AutoSessionLoad();
+  const autoSave = new AutoSessionSave();
 },false);
 
 /* EdittingSwitch
@@ -18,6 +22,8 @@ class EdittingSwitch{
     console.log(this.isEditing);
     this.init(flg);
   }
+  /* 初期化
+  ------------------------------------------------------ */
   init(flg){
     // 起動時に編集可能状態にする
     if (flg) {
@@ -29,6 +35,8 @@ class EdittingSwitch{
       document.querySelector('.input-field[autofocus]').focus();
     }
   }
+  /* 編集可否を切り替える
+  ------------------------------------------------------ */
   switch(){
     console.log('switch()', this);
     if(this.isEditing.checked) {
@@ -40,5 +48,40 @@ class EdittingSwitch{
         elem.contentEditable = false;
       });
     }
+  }
+}
+/* AutoSave
+ 各入力領域の内容を自動保存する
+---------------------------------------------------------------------------- */
+class AutoSessionSave{
+  constructor(){
+    this.inputFields = document.querySelectorAll('.input-field');
+    this.inputFields.forEach(inputField=>{
+      inputField.addEventListener('input', ()=>{this.save()});
+      // 場合によっては'change'やkeyup
+    });
+  }
+  save(){
+    console.log('save()');
+    console.log(document.activeElement.id ,document.activeElement.innerText);
+    if(document.activeElement.id) {
+      console.log('hage');
+      sessionStorage.setItem(document.activeElement.id, document.activeElement.innerText);
+    } 
+  }
+}
+
+/* AutoLoad
+ 各入力領域の内容を自動読み込みする
+---------------------------------------------------------------------------- */
+class AutoSessionLoad{
+  constructor(){
+    this.inputFields = document.querySelectorAll('.input-field');
+    this.inputFields.forEach(inputField=>{
+      inputField.innerText = this.load(inputField.id);
+    });
+  }
+  load(id){
+    return sessionStorage.getItem(id);
   }
 }
